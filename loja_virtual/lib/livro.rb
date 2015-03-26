9dc49e52e979c7c9ac0098038073d505c6e31f0b
dcmspe <dcms.pe@gmail.com>
2015-03-26 16:54:31 -0300

    Alterações 26/03/2015

--- loja_virtual/lib/livro.rb
+++ loja_virtual/lib/livro.rb
@@ -1,5 +1,34 @@
+# encoding: utf-8
+
 class Livro
-	def initialize(autor, isbn = "1", numero_de_paginas)
-		puts "Autor: #{autor}, ISBN: #{isbn}, Paginas: ${numero_de_paginas}"
+	def initialize(autor, isbn = "1", numero_de_paginas, preco)
+		@autor = autor
+		@isbn = isbn
+		@numero_de_paginas = numero_de_paginas
+		@preco = preco
 	end
+
+	def to_s
+		"Autor: #{@autor}, ISBN: #{@isbn}, Páginas: #{@numero_de_paginas}, Preço: #{@preco}"
+	end
+
+	attr_accessor :preco
+=begin
+	attr_writer :preco
+	attr_reader :preco
+=end
+
+=begin 
+	
+	def preco
+		@preco
+	end
+
+	def preco=(preco)
+		@preco = preco
+	end
+
+=end
+
+	
 end
